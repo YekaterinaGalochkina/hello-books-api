7cd50093f3a06ac8026404ed65507d6500f01648
Yekaterina Galochkina <galocichina.ecaterina@gmail.com>
2025-04-30 21:42:58 -0700

    Refactor the code for creating a Book model in the create_book route to use from_dict

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,17 +1,21 @@
 from flask import Blueprint, abort, make_response, request, Response
 from app.models.book import Book
 from ..db import db
-#from app.models.book import books
+
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
 @books_bp.post("")
 def create_book():
     request_body = request.get_json()
-    title = request_body["title"]
-    description = request_body["description"]
 
-    new_book = Book(title=title, description=description)
+    try:
+        new_book = Book.from_dict(request_body)
+
+    except KeyError as error:
+        response = {"message": f"Invalid request: missing {error.args[0]}"}
+        abort(make_response(response, 400))
+
     db.session.add(new_book)
     db.session.commit()
 
@@ -95,40 +99,3 @@ def delete_book(book_id):
     db.session.commit()
 
     return Response(status=204, mimetype="application/json")
-
-# @books_bp.get("")
-# def get_all_books():
-#     books_response = []
-#     for book in books:
-#         books_response.append(
-#             {
-#                 "id": book.id,
-#                 "title": book.title,
-#                 "description": book.description
-#             }
-#         )
-#     return books_response
-
-# @books_bp.get("/<book_id>")
-# def get_one_book(book_id):
-#     book = validate_book(book_id)
-
-#     return {
-#             "id": book.id,
-#             "title": book.title,
-#             "description": book.description,
-#     }
-
-# def validate_book(book_id):
-#     try:
-#         book_id = int(book_id)
-#     except:
-#         response = {"message": f"book {book_id} invalid"}
-#         abort(make_response(response, 400))
-    
-#     for book in books:
-#         if book.id == book_id:
-#             return book
-        
-#     response = {"message": f"book {book_id} not found"}
-#     abort(make_response(response, 404))
