02b0c2a459a4ce504d8b631fc98c8cdd1e626c86
Yekaterina Galochkina <galocichina.ecaterina@gmail.com>
2025-05-05 22:41:34 -0700

    POSTing a Book to an Author and GETting All Books from an Author

--- app/models/book.py
+++ app/models/book.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import ForeignKey
 from typing import Optional
 from ..db import db
+from .author import Author
 
 class Book(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
@@ -16,15 +17,20 @@ class Book(db.Model):
         book_as_dict["title"] = self.title
         book_as_dict["description"] = self.description
 
+        if self.author:
+            book_as_dict["author"] = self.author.name
+
         return book_as_dict
     
     @classmethod
     def from_dict(cls, book_data):
-        new_book = cls(title=book_data["title"],
-                    description=book_data["description"])
-        # We could also use `Book` in place of the `cls` keyword  
-        # The following declaration is equivalent to the one above
-        # new_book = Book(title=book_data["title"],
-        #                 description=book_data["description"])
+        # Use get() to fetch values that could be undefined to avoid raising an error
+        author_id = book_data.get("author_id")
+
+        new_book = cls(
+            title=book_data["title"],
+            description=book_data["description"],
+            author_id=author_id
+        )
 
         return new_book
