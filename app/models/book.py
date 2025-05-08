96953d0f086238cd452bd614190b14118062db0c
Yekaterina Galochkina <galocichina.ecaterina@gmail.com>
2025-05-07 20:22:22 -0700

    Implementing Genre Model, plus endpoints

--- app/models/book.py
+++ app/models/book.py
@@ -1,8 +1,8 @@
+# app/models/book.py
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import ForeignKey
 from typing import Optional
 from ..db import db
-from .author import Author
 
 class Book(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
@@ -10,7 +10,8 @@ class Book(db.Model):
     description: Mapped[str]
     author_id: Mapped[Optional[int]] = mapped_column(ForeignKey("author.id"))
     author: Mapped[Optional["Author"]] = relationship(back_populates="books")
-
+    genres: Mapped[list["Genre"]] = relationship(secondary="book_genre", back_populates="books")
+    
     def to_dict(self):
         book_as_dict = {}
         book_as_dict["id"] = self.id
@@ -20,17 +21,22 @@ class Book(db.Model):
         if self.author:
             book_as_dict["author"] = self.author.name
 
+        if self.genres:
+            book_as_dict["genres"] = [genre.name for genre in self.genres]
+
         return book_as_dict
     
     @classmethod
     def from_dict(cls, book_data):
-        # Use get() to fetch values that could be undefined to avoid raising an error
+    # Use get() to fetch values that could be undefined to avoid raising an error
         author_id = book_data.get("author_id")
+        genres = book_data.get("genres", [])
 
         new_book = cls(
             title=book_data["title"],
             description=book_data["description"],
-            author_id=author_id
+            author_id=author_id,
+            genres=genres
         )
 
         return new_book
